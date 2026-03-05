534978381bd6063c7f63dc23e6671ed83d1057d5
manishgowda17 <manishgowdru17@gmail.com>
2026-03-05 17:23:56 +0530

    Add files via upload

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Author:Manish
+#Date:18th-Feb
+#Version:v1
+#
+#######
+#This script will report the AWS resource usage
+set -x
+#AWS S3
+#AWS EC2
+#AWS Lambda
+#AWS IAM Users
+#
+#
+#list s3 buckets
+echo "Print list of s3 buckets"
+aws s3 ls
+
+#list EC2 Instances
+echo "Print list of ec2  buckets"
+aws ec2 describe-instances
+
+#list lambda
+echo "Print list of lambda functions"
+aws lambda list-functions
+
+#list IAM Users
+echo "Print list of IAM Users"
+aws iam list-users
